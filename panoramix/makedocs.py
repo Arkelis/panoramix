1b891230384d37b1ca96d7ea6102da6b8b2f955a
Arkelis <guillaume.fayard@pycolore.fr>
2019-03-17 00:49:07 +0100

    Recherche auto des fichiers TeX

--- panoramix/makedocs.py
+++ panoramix/makedocs.py
@@ -1,3 +1,4 @@
+import glob
 import click
 import re
 import os
@@ -9,18 +10,21 @@ from . import panoramix
 def makedocs():
     """Utilitaire pour convertir le Mémo Python LaTeX en fichiers RST pour Sphinx."""
     click.echo("Je sors ma potion pour fabriquer la doc !\n")
-    with open("/media/donnees/Documents/programmation/python/projets/panoramix/python.tex", "r") as f:
-        tex = f.read()
-        print("Recherche des \\paragraph{}...")
-        new_tex = re.sub(r"\\paragraph\*?\{(?P<titre>.*?)\}", r"\\textbf{\g<titre>~:}", tex)
-    if tex != new_tex:
-        with open("temp.tex", "w") as f:
-            click.echo("Création d'un fichier TeX temporaire...")
-            f.write(new_tex)
-    else:
-        click.echo("Rien à faire !")
-    click.echo("Conversion en RST...")
-    sh.pandoc("python.tex", o="/media/donnees/Documents/programmation/python/memo/rst/python.rst")
+    for path in glob.iglob("**/*.tex", recursive=True):
+        with open(path, "r") as f:
+            tex = f.read()
+            print("Recherche des \\paragraph{}...")
+            new_tex = re.sub(r"\\paragraph\*?\{(?P<titre>.*?)\}", r"\\textbf{\g<titre>~:}", tex)
+        if tex != new_tex:
+            with open("temp.tex", "w") as f:
+                click.echo("Création d'un fichier TeX temporaire...")
+                f.write(new_tex)
+                to_convert = "temp.tex"
+        else:
+            click.echo("Rien à faire !")
+            to_convert = path
+        click.echo("Conversion en RST...")
+        sh.pandoc(to_convert, o=path[:-4] + ".rst")
     click.echo("Nettoyage du fichier temporaire...")
     sh.rm("temp.tex")
     click.echo("Terminé.\n")
