61c9d830025d28bfe1ad6608eae1fe01aab200bf
Arkelis <guillaume.fayard@pycolore.fr>
2019-05-06 19:48:58 +0200

    Amélioration vérification du script d'installation

--- panoramix/composer.py
+++ panoramix/composer.py
@@ -1,10 +1,9 @@
-import glob
 import click
-import re
-import os
 import subprocess
+import requests
+import hashlib
 
-from . import panoramix, success, run_process
+from . import panoramix, success, run_process, abort
 
 @panoramix.group()
 def composer():
@@ -18,7 +17,12 @@ def install():
     click.secho("Téléchargement de l'installateur...", bold=True, fg="cyan")
     run_process(["php", "-r", "copy('https://getcomposer.org/installer', 'composer-setup.php');"])
     click.secho("Véfification du téléchargement...", bold=True, fg="cyan")
-    run_process(["php", "-r", "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"])
+    downloaded_sig = hashlib.sha384(open("composer-setup.php").read().encode()).hexdigest()
+    trusted_sig = requests.get("https://composer.github.io/installer.sig").text.replace("\n", "")
+    if downloaded_sig == trusted_sig:
+        click.secho("Vérifié !")
+    else:
+        abort("Les signatures sont différentes.")
     click.secho("Génération de composer...", bold=True, fg="cyan")
     run_process(["sudo", "php", "composer-setup.php", "--install-dir", "/usr/bin", "--filename", "composer"])
     click.secho("Nettoyage...", bold=True, fg="cyan")
